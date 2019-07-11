5e72dbf6fbe2afe1cdb094525aca4a1363acfa9e
Deyvison Silas <deyvisonsilas122@gmail.com>
2019-07-10 23:34:14 -0300

    Entities instantiated and created loop to product registration

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using product_order.Entities;
+using product_order.Entities.Enums;
 
 namespace product_order
 {
@@ -6,7 +8,47 @@ namespace product_order
     {
         static void Main(string[] args)
         {
-            Console.WriteLine();
+            Console.WriteLine("Enter client data:");
+
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+
+            Console.Write("E-mail: ");
+            string email = Console.ReadLine();
+
+            Console.Write("Birth date (MM/DD/YYYY): ");
+            DateTime birthDate = Convert.ToDateTime(Console.ReadLine());
+
+            Client client = new Client(name, email, birthDate);
+
+            Console.WriteLine("Enter order data: ");
+
+            Console.Write("Status: ");
+            OrderStatus status = Enum.Parse<OrderStatus>(Console.ReadLine());
+            
+            Order order = new Order(DateTime.Now, status, client);
+
+            Console.Write("How many items to this order? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for(int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Enter the {(i + 1)} item data:", i);
+
+                Console.Write("Product name: ");
+                string prodName = Console.ReadLine();
+
+                Console.Write("Product price: ");
+                double prodPrice = double.Parse(Console.ReadLine());
+
+                Product product = new Product(prodName, prodPrice);
+
+                Console.Write("Quantity: ");
+                int prodAmount = int.Parse(Console.ReadLine());
+
+                OrderItem orderItem = new OrderItem(prodAmount, prodPrice);
+            }
+
         }
     }
 }
