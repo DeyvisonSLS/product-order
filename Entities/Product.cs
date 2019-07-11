57d8d1b3d7888fae43643ea2d65d504b99a84b34
Deyvison Silas <deyvisonsilas122@gmail.com>
2019-07-10 23:18:58 -0300

    Product builders created

--- Entities/Product.cs
+++ Entities/Product.cs
@@ -7,5 +7,13 @@ namespace product_order.Entities
         public string Name { get; set; }
         //This price is a dinamic value, here the price will be changed acordingly to the market.
         public double Price { get; set; }
+        public Product()
+        {
+        }
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
     }
 }
